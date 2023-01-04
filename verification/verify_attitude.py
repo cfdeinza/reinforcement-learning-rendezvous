9da1077169306d1121e279895e87e75a445d47a2
De Inza Niemeijer, Carlos Francisco <cfdeinza@gmail.com>
2023-01-04 17:51:04 +0100

    Updated verification scripts
    Made better plots. Still need some reference data to compare attitude dynamics.

--- verification/verify_attitude.py
+++ verification/verify_attitude.py
@@ -4,7 +4,10 @@ In order to do this, I created a simle class `RigidBody`
 """
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 from utils.dynamics import derivative_of_att_and_rot_rate
+from utils.environment_utils import make_new_env
+from utils.quaternions import quat_error, quat2mat
 
 
 class RigidBody:
@@ -17,8 +20,10 @@ class RigidBody:
         """
         self.q = None  # Current attitude of the body (initialized on reset() call)
         self.w = None  # Current rot rate of the body (initialized on reset() call)
-        self.inertia = 0  # Moment of inertia of the body [kg.m2]
-        self.inv_inertia = 0  # Inverse of the moment of inertia of the body
+        self.mc = 100
+        self.lc = 1
+        self.inertia = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]) * 1 / 12 * self.mc * (2 * self.lc ** 2)
+        self.inv_inertia = np.linalg.inv(self.inertia)
         self.q0 = q0  # Initial attitude of the body
         self.w0 = w0  # Initial rot rate of the body [rad/s]
         return
@@ -61,18 +66,178 @@ class RigidBody:
         return
 
 
-def main():
-    q0 = np.array([1, 0, 0, 0])     # Initial attitude of the body (relative to an inertial frame)
-    w0 = np.array([0, 0, 0])        # Initial rot rate of the body (expressed in body frame) [rad/s]
-    body = RigidBody(q0, w0)        # create an instance of the body
-    body.reset()                    # reset the state of the body
-    dt = 1                          # Time interval to propagate each time integrate() is called [s]
-    torque = np.array([0, 0, 0])    # Torque applied on the body [N.m]
-    body.integrate(dt, torque)
+def run_simulation(env):
+    expected_timesteps = int(env.t_max / env.dt)
+    t = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    qc = np.full(shape=(expected_timesteps, 4), fill_value=np.nan)
+    wc = np.full(shape=(expected_timesteps, 3), fill_value=np.nan)
+    sol_qc = np.full(shape=(expected_timesteps, 4), fill_value=np.nan)
+    sol_wc = np.full(shape=(expected_timesteps, 3), fill_value=np.nan)
 
+    env.reset()
+    body = RigidBody(q0=env.qc, w0=env.wc)  # create an instance of the body
+    body.reset()  # reset the state of the body
+
+    k = 0
+    while env.t < env.t_max:
+        t[k] = env.t
+        qc[k, :] = env.qc
+        wc[k, :] = env.wc
+        sol_qc[k, :] = body.q
+        sol_wc[k, :] = body.w
+        if env.t < env.t_max/2:
+            action = np.array([0, 0, 0, 0, 0, 0]) + 1
+        else:
+            action = np.array([0, 0, 0, 0, 0, 1]) + 1
+        env.step(action)
+        body_torque = (action[3:] - 1) * env.torque  # Torque applied on the body [N.m]
+        body.integrate(env.dt, body_torque)
+        k += 1
+
+    # Errors:
+    # print(f"Final quat error: {np.abs(qc[-1] - sol_qc[-1])}")
+    q_error = quat_error(q_est=qc[-1], q_true=sol_qc[-1])
+    theta = 2*np.arccos(q_error[0])
+    print(f"Final attitude error: {np.degrees(theta)} deg")
+    print(f"Or: {np.degrees(2*np.arccos(1-np.abs(qc[-1, 0] - sol_qc[-1, 0])))}")
+    w_error = sol_wc[-1] - wc[-1]
+    print(f"Final rot rate error: {np.degrees(np.linalg.norm(w_error))} deg/s")
+
+    out = dict(
+        t=t,
+        qc=qc,
+        wc=wc,
+        sol_qc=sol_qc,
+        sol_wc=sol_wc,
+    )
+    return out
+
+
+def plot_results(data: dict) -> None:
+    t = data.get("t")
+    qc = data.get("qc")
+    wc = data.get("wc")
+    sol_qc = data.get("sol_qc")
+    sol_wc = data.get("sol_wc")
+    n_rows = 2
+    n_cols = 4
+    fig, ax = plt.subplots(n_rows, n_cols, figsize=(10, 5))
+    # Plot numerical:
+    ax[0, 0].plot(t, qc[:, 0], label="numerical")
+    ax[0, 1].plot(t, qc[:, 1], label="numerical")
+    ax[0, 2].plot(t, qc[:, 2], label="numerical")
+    ax[0, 3].plot(t, qc[:, 3], label="numerical")
+    ax[1, 0].plot(t, wc[:, 0], label="numerical")
+    ax[1, 1].plot(t, wc[:, 1], label="numerical")
+    ax[1, 2].plot(t, wc[:, 2], label="numerical")
+    # Plot analytical:
+    ax[0, 0].plot(t, sol_qc[:, 0], "--", linewidth=1, label="analytical")
+    ax[0, 1].plot(t, sol_qc[:, 1], "--", label="analytical")
+    ax[0, 2].plot(t, sol_qc[:, 2], "--", label="analytical")
+    ax[0, 3].plot(t, sol_qc[:, 3], "--", label="analytical")
+    ax[1, 0].plot(t, sol_wc[:, 0], "--", label="analytical")
+    ax[1, 1].plot(t, sol_wc[:, 1], "--", label="analytical")
+    ax[1, 2].plot(t, sol_wc[:, 2], "--", label="analytical")
+
+    max_q = 1.1
+    max_w = max(1e-6, np.max(np.abs(wc))) * 1.1
+
+    y_labels = [
+        ["$q_w$", "$q_x$", "$q_y$", "$q_z$"],
+        ["$\omega_x$ [rad/s]", "$\omega_y$ [rad/s]", "$\omega_z$ [rad/s]", ""],
+    ]
+
+    for row in range(n_rows):
+        for col in range(n_cols):
+            ax[row, col].grid()
+            ax[row, col].legend()
+            ax[row, col].set_xlim([t[0], t[-1]])
+            # print(row, col)
+            ax[row, col].set_ylabel(y_labels[row][col])
+            if row == 0:
+                lim = max_q
+                ax[row, col].set_ylim([-lim, lim])
+            else:
+                lim = max_w
+                ax[row, col].set_ylim([-lim, lim])
+
+    plt.show()
+    plt.close()
+
+    fig2, ax2 = plt.subplots(1, 1, figsize=(10, 5))
+    # Integration error:
+    ax2.plot(t, np.abs(qc[:, 0] - sol_qc[:, 0]), label="$qc_w$ [-]")
+    ax2.plot(t, np.abs(qc[:, 1] - sol_qc[:, 1]), label="$qc_x$ [-]")
+    ax2.plot(t, np.abs(qc[:, 2] - sol_qc[:, 2]), label="$qc_y$ [-]")
+    ax2.plot(t, np.abs(qc[:, 3] - sol_qc[:, 3]), label="$qc_z$ [-]")
+    ax2.plot(t, np.abs(wc[:, 0] - sol_wc[:, 0]), label="$wc_x$ [rad/s]")
+    ax2.plot(t, np.abs(wc[:, 1] - sol_wc[:, 1]), label="$wc_y$ [rad/s]")
+    ax2.plot(t, np.abs(wc[:, 2] - sol_wc[:, 2]), label="$wc_z$ [rad/s]")
+    ax2.set_xlabel("Time [s]")
+    ax2.set_xlim([t[0], t[-1]])
+    ax2.set_ylabel("Error")
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.title("Integration errors")
+    plt.show()
+    plt.close()
+
+    _ = plt.figure(num='3D', clear=True, figsize=(10, 5))
+    ax3 = plt.axes(projection="3d")
+    lvlh_len = 1
+    ax3.plot3D([0, lvlh_len], [0, 0], [0, 0], "r-", label="$X_{LVLH}$")
+    ax3.plot3D([0, 0], [0, lvlh_len], [0, 0], "g-", label="$Y_{LVLH}$")
+    ax3.plot3D([0, 0], [0, 0], [0, lvlh_len], "b-", label="$Z_{LVLH}$")
+    # ax3.plot3D(x, y, z, label=f"Trajectory in {t[-1]} s")  # , mec="k", ms=10, alpha=0.8)
+    # ax3.plot3D(sol_x, sol_y, sol_z, "--", label="Analytical")
+    # ax3.plot3D(x[0], y[0], z[0], "kx", label="Chaser start")
+    # ax3.plot3D(x[-1], y[-1], z[-1], "g*", label="Chaser end")
+    # ax3.plot3D([0], [0], [0], "r.", label="Target")
+    ax3.set_box_aspect((1, 1, 1))
+    lim = 2
+    ax3.set_xlim([-lim, lim])
+    ax3.set_ylim([-lim, lim])
+    ax3.set_zlim([-lim, lim])
+    ax3.set_zlabel("$Z_{LVLH}$ (m)")
+    plt.xlabel('$X_{LVLH}$ (m)'), plt.ylabel('$Y_{LVLH}$ (m)')
+    ax3.set_title("Rotational motion")
+    ax3.legend()
+    plt.show()
+    plt.close()
     return
 
 
+def main(env):
+    results = run_simulation(env)
+    plot_results(results)
+    return
+
+
+def chaser2lvlh(qc, vec):
+    assert vec.shape == (3,), f"Vector must have a (3,) shape, but has {vec.shape}"
+    return np.matmul(quat2mat(qc), vec)
+
+
 if __name__ == "__main__":
-    main()
+    config = dict(
+        rc0=np.array([0, -10, 0]),
+        vc0=np.array([0, 0, 0]),
+        qc0=np.array([1, 0, 0, 0]),
+        wc0=np.radians(np.array([0, 0, 0])),
+        qt0=np.array([1, 0, 0, 0]),
+        wt0=np.array([0, 0, 0]),
+        rc0_range=0,
+        vc0_range=0,
+        qc0_range=0,
+        wc0_range=0,
+        qt0_range=0,
+        wt0_range=0,
+        bt=1,
+        dt=1,
+        t_max=360*2.1,
+        quiet=True,
+    )
+    eval_env = make_new_env(config)
+    main(eval_env)
     # TODO: find some reference data to compare it with.
--- verification/verify_cw.py
+++ verification/verify_cw.py
@@ -3,105 +3,217 @@ Verify my implementation of the CW model.
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from math import ceil
 from utils.dynamics import clohessy_wiltshire_solution
-from rendezvous_env import RendezvousEnv
+from utils.environment_utils import make_new_env
+# from rendezvous_env import RendezvousEnv
 
 
-def run_simulation(env, orbits=1):
+def run_simulation(env):
+    expected_timesteps = int(env.t_max / env.dt)
+    t = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    x = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    y = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    z = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    u = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    v = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    w = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_x = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_y = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_z = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_u = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_v = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+    sol_w = np.full(shape=(expected_timesteps,), fill_value=np.nan)
+
     env.reset()
-    dt = env.dt  # time step interval
-    n = env.n  # mean motion
-    orbital_period = 2 * np.pi / n  # orbital period [s]
-    print(f"dt = {dt}")
-    print(f"n = {n} (h = {env.h})")
-    print(f"T = {orbital_period}")
-    t = 0
-    t_max = orbital_period * orbits
-    rc = np.array([10, -10, 10])
-    vc = np.array([0, 0, 0])
-    ts = [0]
-    rcs = [rc]
-    vcs = [vc]
 
-    while t < t_max:
-        rc, vc = clohessy_wiltshire_solution(rc, vc, n, dt)
-        t += dt
-        ts.append(t)
-        rcs.append(rc)
-        vcs.append(vc)
+    k = 0
+    pos_0, vel_0 = env.rc, env.vc
+    new_pos, new_vel = pos_0, vel_0
+    while env.t < env.t_max:
+        t[k] = env.t
+        x[k], y[k], z[k] = env.rc
+        u[k], v[k], w[k] = env.vc
+        action = np.ones(shape=(6,))
+        env.step(action)
+        sol_x[k], sol_y[k], sol_z[k] = new_pos
+        sol_u[k], sol_v[k], sol_w[k] = new_vel
+        # new_pos, new_vel = clohessy_wiltshire_solution(new_pos, new_vel, n=env.n, t=env.dt)
+        new_pos, new_vel = clohessy_wiltshire_solution(pos_0, vel_0, n=env.n, t=env.t)
+        k += 1
 
-    # Convert the lists to arrays:
-    ts = np.array(ts)
-    rcs = np.array(rcs)
-    vcs = np.array(vcs)
+    # Compute terminal integration error:
+    print(f"Terminal integration errors:")
+    print(f"rx: {np.abs(x[-1] - sol_x[-1])} m")
+    print(f"ry: {np.abs(y[-1] - sol_y[-1])} m")
+    print(f"rz: {np.abs(z[-1] - sol_z[-1])} m")
+    print(f"vx: {np.abs(u[-1] - sol_u[-1])} m/s")
+    print(f"vy: {np.abs(v[-1] - sol_v[-1])} m/s")
+    print(f"vz: {np.abs(w[-1] - sol_w[-1])} m/s")
+    r_error = np.sqrt((x - sol_x)**2 + (y - sol_y)**2 + (z - sol_z)**2)
+    v_error = np.sqrt((u - sol_u)**2 + (v - sol_v)**2 + (w - sol_w)**2)
+    print(f"r: {r_error[-1]} m")
+    print(f"v: {v_error[-1]} m/s")
 
     # Save the results in a dictionary:
     out = dict(
-        ts=ts,
-        rcs=rcs,
-        vcs=vcs,
+        t=t,
+        x=x,
+        y=y,
+        z=z,
+        u=u,
+        v=v,
+        w=w,
+        sol_x=sol_x,
+        sol_y=sol_y,
+        sol_z=sol_z,
+        sol_u=sol_u,
+        sol_v=sol_v,
+        sol_w=sol_w,
     )
     return out
 
 
 def plot_results(data: dict) -> None:
-    t = data.get("ts")
-    rc = data.get("rcs")
-    vc = data.get("vcs")
-    x = rc[:, 0]
-    y = rc[:, 1]
-    z = rc[:, 2]
-    u = vc[:, 0]
-    v = vc[:, 1]
-    w = vc[:, 2]
-    fig, ax = plt.subplots(2, 3, figsize=(10, 5))
-    ax[0, 0].plot(t, x, label="rx")
-    ax[0, 1].plot(t, y, label="ry")
-    ax[0, 2].plot(t, z, label="rz")
-    ax[1, 0].plot(t, u, label="vx")
-    ax[1, 1].plot(t, v, label="vy")
-    ax[1, 2].plot(t, w, label="vz")
+    t = data.get("t")
+    x = data.get("x")
+    y = data.get("y")
+    z = data.get("z")
+    u = data.get("u")
+    v = data.get("v")
+    w = data.get("w")
+    sol_x = data.get("sol_x")
+    sol_y = data.get("sol_y")
+    sol_z = data.get("sol_z")
+    sol_u = data.get("sol_u")
+    sol_v = data.get("sol_v")
+    sol_w = data.get("sol_w")
+    n_rows = 2
+    n_cols = 3
+    fig, ax = plt.subplots(n_rows, n_cols, figsize=(10, 5))
+    # Plot numerical:
+    ax[0, 0].plot(t, x, label="numerical")
+    ax[0, 1].plot(t, y, label="numerical")
+    ax[0, 2].plot(t, z, label="numerical")
+    ax[1, 0].plot(t, u, label="numerical")
+    ax[1, 1].plot(t, v, label="numerical")
+    ax[1, 2].plot(t, w, label="numerical")
+    # Plot analytical:
+    ax[0, 0].plot(t, sol_x, "--", linewidth=1, label="analytical")
+    ax[0, 1].plot(t, sol_y, "--", label="analytical")
+    ax[0, 2].plot(t, sol_z, "--", label="analytical")
+    ax[1, 0].plot(t, sol_u, "--", label="analytical")
+    ax[1, 1].plot(t, sol_v, "--", label="analytical")
+    ax[1, 2].plot(t, sol_w, "--", label="analytical")
+    # ax[0, 0].plot(t, np.abs(x - sol_x), "--", linewidth=1, label="error")
+    # ax[0, 1].plot(t, np.abs(y - sol_y), "--", label="error")
+    # ax[0, 2].plot(t, np.abs(z - sol_z), "--", label="error")
+    # ax[1, 0].plot(t, np.abs(u - sol_u), "--", label="error")
+    # ax[1, 1].plot(t, np.abs(v - sol_v), "--", label="error")
+    # ax[1, 2].plot(t, np.abs(w - sol_w), "--", label="error")
 
-    for i in range(2):
-        for j in range(3):
-            ax[i, j].grid()
-            ax[i, j].legend()
-            ax[i, j].set_xlim([t[0], t[-1]])
-            if i == 0:
-                lim = np.abs(rc).max()
-                ax[i, j].set_ylim([-lim, lim])
+    max_xyz = np.max(np.abs(np.hstack((x, y, z))))
+    max_uvw = np.max(np.abs(np.hstack((u, v, w))))
+
+    y_labels = [
+        ["$r_x$ [m]", "$r_y$ [m]", "$r_z$ [m]"],
+        ["$v_x$ [m/s]", "$v_y$ [m/s]", "$v_z$ [m/s]"],
+    ]
+
+    for row in range(n_rows):
+        for col in range(n_cols):
+            ax[row, col].grid()
+            ax[row, col].legend()
+            ax[row, col].set_xlim([t[0], t[-1]])
+            ax[row, col].set_ylabel(y_labels[row][col])
+            if row == 0:
+                lim = max_xyz
+                ax[row, col].set_ylim([-lim, lim])
             else:
-                lim = np.abs(vc).max()
-                ax[i, j].set_ylim([-lim, lim])
+                lim = max_uvw
+                ax[row, col].set_ylim([-lim, lim])
 
     plt.show()
     plt.close()
 
+    fig2, ax2 = plt.subplots(1, 1, figsize=(10, 5))
+    # Integration error:
+    ax2.plot(t, np.abs(x - sol_x), label="$r_x$ [m]")
+    ax2.plot(t, np.abs(y - sol_y), label="$r_y$ [m]")
+    ax2.plot(t, np.abs(z - sol_z), label="$r_z$ [m]")
+    ax2.plot(t, np.abs(u - sol_u), label="$v_x$ [m/s]")
+    ax2.plot(t, np.abs(v - sol_v), label="$v_y$ [m/s]")
+    ax2.plot(t, np.abs(w - sol_w), label="$v_z$ [m/s]")
+    ax2.set_xlabel("Time [s]")
+    ax2.set_xlim([t[0], t[-1]])
+    ax2.set_ylabel("Error")
+    # ax2.set_ylim(bottom=0, top=None)
+    plt.legend()
+    plt.grid()
+    plt.title("Integration errors")
+    plt.show()
+    plt.close()
+
     _ = plt.figure(num='3D', clear=True, figsize=(10, 5))
-    ax2 = plt.axes(projection="3d")
-    ax2.plot3D(x, y, z, label=f"Trajectory in {t[-1]} s")  # , mec="k", ms=10, alpha=0.8)
-    ax2.plot3D(x[0], y[0], z[0], "kx", label="Chaser start")
-    ax2.plot3D(x[-1], y[-1], z[-1], "g*", label="Chaser end")
-    ax2.plot3D([0], [0], [0], "ro", label="Target")
-    # ax2.set_aspect('equal')
-    ax2.set_box_aspect((1, 1, 1))
-    lim = np.abs(rc).max()
-    ax2.set_xlim([-lim, lim])
-    ax2.set_ylim([-lim, lim])
-    ax2.set_zlim([-lim, lim])
-    ax2.set_zlabel("$Z_{LVLH}$ (m)")
+    ax3 = plt.axes(projection="3d")
+    ax3.plot3D(x, y, z, label=f"Trajectory in {t[-1]} s")  # , mec="k", ms=10, alpha=0.8)
+    ax3.plot3D(sol_x, sol_y, sol_z, "--", label="Analytical")
+    ax3.plot3D(x[0], y[0], z[0], "kx", label="Chaser start")
+    ax3.plot3D(x[-1], y[-1], z[-1], "g*", label="Chaser end")
+    ax3.plot3D([0], [0], [0], "r.", label="Target")
+    ax3.set_box_aspect((1, 1, 1))
+    lim = ceil(max_xyz)
+    ax3.set_xlim([-lim, lim])
+    ax3.set_ylim([-lim, lim])
+    ax3.set_zlim([-lim, lim])
+    ax3.set_zlabel("$Z_{LVLH}$ (m)")
     plt.xlabel('$X_{LVLH}$ (m)'), plt.ylabel('$Y_{LVLH}$ (m)')
-    ax2.set_title("Chaser position")
-    ax2.legend()
+    ax3.set_title("Chaser position")
+    ax3.legend()
     plt.show()
     plt.close()
 
     return
 
 
-if __name__ == "__main__":
-    eval_env = RendezvousEnv(quiet=True)
-    results = run_simulation(eval_env, orbits=1)
-    plot_results(results)
+def main(args):
+    # eval_env = RendezvousEnv(quiet=True)
+    mu = 3.986004418e14
+    n = np.sqrt(mu / (800e3 + 6371e3) ** 3)
+    p = 2*np.pi / n
+    t_max = int(p/8)
+    config = dict(
+        rc0=np.array([0, -10, 1]),
+        vc0=np.array([-0.01, 0.01, 0]),
+        qc0=np.array([1, 0, 0, 0]),
+        wc0=np.array([0, 0, 0]),
+        qt0=np.array([1, 0, 0, 0]),
+        wt0=np.array([0, 0, 0.000001]),
+        rc0_range=0,
+        vc0_range=0,
+        qc0_range=0,
+        wc0_range=0,
+        qt0_range=0,
+        wt0_range=0,
+        dt=1,
+        t_max=t_max,  # 60*30,
+        quiet=True,
+    )
+    eval_env = make_new_env(config)
+    print(f"Integrating over {t_max} seconds ({round(t_max/60, 3)} minutes) ({round(t_max/p, 3)} orbits)")
+    results = run_simulation(eval_env)
+    if args.show:
+        plot_results(results)
+    return
 
+
+class Arguments:
+    def __init__(self, show=True):
+        self.show = show
+        return
+
+
+if __name__ == "__main__":
+    arguments = Arguments(show=True)
+    main(arguments)
     # TODO: get some reference data to compare with.
